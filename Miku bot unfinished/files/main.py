ea27b4a90dd824b906698df73cb1df22392cb5c9
Blue-hubtech <nelsonehindero@gmail.com>
2026-05-22 22:11:10 +0100

    Delete Miku bot unfinished/files directory

--- Miku bot unfinished/files/main.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import os
-import runpy
-
-
-APP_DIR = Path(__file__).parent / "Miku bot unfinished" / "files"
-
-os.chdir(APP_DIR)
-runpy.run_path("bot.py", run_name="__main__")
